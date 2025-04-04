eaa6c3f311a1acb2159e1c779b441b73d9f4942f
LilyS222 <soomin.25641@gmail.com>
2025-04-04 12:04:58 +0900

    feat: 프론트엔드 환경 설정

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+      <div className="container">
+        <header>
+          <h1>미세멍지 🐶</h1>
+          <p>미세먼지 속 우리 반려견을 위한 웹 서비스</p>
+        </header>
+        <main>
+          {/* 여기에 지도, 추천 장소, 행동 가이드 등 추가 */}
+        </main>
+      </div>
+  );
+}
+export default App;
